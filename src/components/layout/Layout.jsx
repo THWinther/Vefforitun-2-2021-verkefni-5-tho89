5a8f4d572402c1a8df593bce0f67023d7e9cdaa8
THWinther <43626565+THWinther@users.noreply.github.com>
2021-03-23 13:47:14 +0000

    layout er komið

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,10 +2,11 @@ import { render } from "react-dom";
 
 // TODO sækja Sass
 /* todo senda inn efni */
-export function Layout({ }) {
+export function Layout({children}) {
   return(
     <div>
       <h1>Rúv Fréttir</h1>
+      <div>{children}</div>
       <a href="https://www.ruv.is/">Fréttir frá Rúv</a>
     </div>
   ); 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,9 @@
+import { Layout } from "../components/layout/Layout";
 
 export function Index() {
   return (
-  <div>
-    <p>NEWS CATEGORIES</p>
-  </div>
+    <div>
+      <p>NEWS CATEGORIES</p>
+    </div>
   )
 }
